6fd0443b14a3ae3372d5525b0d05199803c87340
Santiago E <santy.e@gmail.com>
2023-09-20 19:54:18 -0600

    Fix

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,30 +1,20 @@
-#!/usr/bin/node
-const request = require('request');
-const url = 'http://swapi.co/api/films/';
-let id = parseInt(process.argv[2], 10);
-let characters = [];
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Status Code:', response.statusCode);
+  } else {
+    const filmData = JSON.parse(body);
+    const characters = filmData.characters;
 
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const results = resp.results;
-    if (id < 4) {
-      id += 3;
-    } else {
-      id -= 3;
-    }
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].episode_id === id) {
-        characters = results[i].characters;
-        break;
-      }
-    }
-    for (let j = 0; j < characters.length; j++) {
-      request(characters[j], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
+    // Itera a través de la lista de personajes e imprímelos uno por uno
+    characters.forEach((characterUrl) => {
+      request(characterUrl, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          const characterData = JSON.parse(body);
+          console.log(characterData.name);
         }
       });
-    }
+    });
   }
 });
