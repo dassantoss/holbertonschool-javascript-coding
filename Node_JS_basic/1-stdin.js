9aebc07c5607b547a2b2df0104dfcd2aa7266f28
Santiago E <santy.e@gmail.com>
2023-10-13 14:59:17 -0600

    Fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-console.log('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 rl.question('', ((name) => {
   console.log(`Your name is: ${name}`);
   console.log('This important software is now closing\n');
