02068794673da2598f88a3cac774c44a60476bfc
Santiago E <santy.e@gmail.com>
2023-10-13 15:20:23 -0600

    Fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-console.log('Welcome to Holberton School, what is your name?');
-rl.question('', ((name) => {
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-}));
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
