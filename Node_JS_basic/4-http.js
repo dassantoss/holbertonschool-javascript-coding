cbef68e8c76bb657759359d2881cb2d3bbd7fbaf
Santiago E <santy.e@gmail.com>
2023-10-14 10:51:21 -0600

    Add small HTTP server using Node's HTTP module

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!\n');
+});
+
+const PORT = 1245;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+module.exports = server;
