88d6079a838885b7e47a417cdc261e1bc0af8a94
Santiago E <santy.e@gmail.com>
2023-10-13 18:51:25 -0600

    Add script that read database file synchronously

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+module.exports = function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const students = data.split('\n')
+      .map((student) => student.split(','))
+      .filter((student) => student.length === 4 && student[0] !== 'firstname')
+      .map((student) => ({
+        firstName: student[0],
+        lastName: student[1],
+        age: student[2],
+        field: student[3],
+      }));
+    const csStudents = students
+      .filter((student) => student.field === 'CS')
+      .map((student) => student.firstName);
+    const sweStudents = students
+      .filter((student) => student.field === 'SWE')
+      .map((student) => student.firstName);
+    console.log(`Number of students: ${students.length}`);
+    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
